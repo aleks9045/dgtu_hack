6afe5d58faaf3557bfa3f20770dcd4cc3f904482
aleks9045 <aleksey9045@gmail.com>
2024-04-20 22:18:21 +0300

    front rtubles

--- api/experts/models.py
+++ api/experts/models.py
@@ -13,7 +13,7 @@ class ExpertModel(Base):
     role = Column(VARCHAR(32), nullable=False)
     hashed_password = Column(VARCHAR(1023), nullable=False)
     photo = Column(VARCHAR(255), nullable=True)
-    company = Column(SmallInteger, ForeignKey('case.id_c'), nullable=False)
+    company = Column(SmallInteger, ForeignKey('case.id_co'), nullable=False)
 
 
 
