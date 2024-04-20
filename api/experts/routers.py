b70258ff41b0a5261f3906390f343496d068a845
aleks9045 <aleksey9045@gmail.com>
2024-04-21 01:53:24 +0300

    model dump

--- api/experts/routers.py
+++ api/experts/routers.py
@@ -202,7 +202,7 @@ async def all_case(session: AsyncSession = Depends(db_session.get_async_session)
     result = await session.execute(
         select(CaseModel.id_ca, CaseModel.name, CaseModel.about, CaseModel.file, CaseModel.company).where(1 == 1))
     for i in result.all():
-        result = await session.execute(select(CompanyModel.name).where(CompanyModel.id_co == i[0]))
+        result = await session.execute(select(CompanyModel.name).where(CompanyModel.id_co == i[4]))
         company_name = result.fetchone()
         print(company_name)
         res_dict.append({"id_ca": i[0],
